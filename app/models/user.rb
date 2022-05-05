03b6762d448082510831fa895f42ba678d058c06
Kate <k8coding@gmail.com>
2022-05-05 03:31:09 +0000

    adds functionality to create users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base 
   
+  validates :email, :username, uniqueness: true
+  validates :email, :avatar_url, :username, :password, presence: true
+
   has_many :finstagram_posts
   has_many :comments
   has_many :likes
