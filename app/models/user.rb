bc7d705ab85d0676922b2d569464e7ef45391ce3
Kate <k8coding@gmail.com>
2022-05-05 19:42:29 +0000

    git commit -m "encrypt password"

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
 class User < ActiveRecord::Base 
   
   validates :email, :username, uniqueness: true
-  validates :email, :avatar_url, :username, :password, presence: true
+  validates :email, :avatar_url, :username, :presence: true
+
+  has_secure_password
 
   has_many :finstagram_posts
   has_many :comments
